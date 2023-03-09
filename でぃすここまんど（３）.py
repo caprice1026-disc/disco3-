45394613d0ef6fa0ffd6d82f2354360bbf1b3a08
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2023-03-10 01:51:37 +0900

    Update でぃすここまんど（３）.py

--- でぃすここまんど（３）.py
+++ でぃすここまんど（３）.py
@@ -8,27 +8,26 @@ from discord import(
 
 from discord.app_commands import CommandTree
 
-TOKEN = "API KEY"
+TOKEN = ""
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 #鯖設定するなら　guild = discord.guild(id=)
 
-
 #ここで確認のUIを作成。どうにかして下の方のコードとリンクさせる必要がある。
 class confomationview(discord.ui.View):
     def __init__(self, timeout=180):
         super().__init__(timeout=timeout)
-        self.send_embed = embed
+        #self.send_embed = embed
         #self.send_url_view = url_view URLview使ってないから消していい
     @discord.ui.button(label="OK", style=discord.ButtonStyle.success)
     async def ok(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await self.channel.send(embed=self.send_embed, view=self.send_url_view)
+        await self.channel.send(embed=self.send_embed)
     @discord.ui.button(label="NG", style=discord.ButtonStyle.gray)
     async def ng(self, button: discord.ui.Button, interaction: discord.Interaction):
         return
-    
+
 
 
 @client.event
@@ -52,7 +51,7 @@ async def promotion_command(interaction: discord.Interaction,role:discord.Role,t
     #rolenameの定義の仕方(問題なし)
     rolename = role.name
     channel = client.get_channel(1042657556386033676)
-    view = confomationview
+    view = confomationview()
     await interaction.response.send_message(f"{rolename}へ{text}  と送信してよいですか？", ephemeral=True)
     await interaction.followup.send(view=view)
 
@@ -61,4 +60,5 @@ client.run(TOKEN)
 #UIについてはこれを参照　https://qiita.com/Broccolingual/items/b008421e535b96c05557
 #UIの組み合わせについてはこれ参照　https://qiita.com/halglobe0108/items/d6794e2104b6c90580a6#%E3%83%A1%E3%83%83%E3%82%BB%E3%83%BC%E3%82%B8%E3%81%AE%E5%9F%8B%E3%82%81%E8%BE%BC%E3%81%BF
 #やること→UIをどうにかすること　他は完成している
+
 
